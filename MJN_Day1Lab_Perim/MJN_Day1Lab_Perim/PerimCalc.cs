ff51ce7240b23e9873c10ccbcfd2cb8f37363d61
Megan J. Norton <go2megan@gmail.com>
2017-06-26 16:05:50 -0400

    Norton_Day1Lab_Perimeters

---
+++ MJN_Day1Lab_Perim/MJN_Day1Lab_Perim/PerimCalc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MJNorton_Day1Lab_Perim
+{
+    class PerimeterCalculator
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+
+            float length;
+            float width;
+
+            Console.WriteLine("Enter Length:");
+
+            length = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Width:");
+
+            width = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area " + (length * width));
+            Console.WriteLine("Perimeter " + ((length * 2) + (width * 2)));
+            Console.WriteLine("Continue? (y/n)");
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
+
