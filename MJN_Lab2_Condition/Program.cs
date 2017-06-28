3f38e8934043a2da0d9c39bcd810d84e626e0c3e
Megan J. Norton <go2megan@gmail.com>
2017-06-27 22:52:52 -0400

    Add files via Git Hub

---
+++ MJN_Lab2_Condition/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MJN_Lab2_Condition
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string name; // the user's name
+            int i; //the value the user enters
+            string answer = "y";
+            {
+                Console.WriteLine("Hi! What is your name?");
+                name = Console.ReadLine();
+            }
+            while (answer == "y")
+            {
+                {
+                    Console.WriteLine("Hi " + name + "! Let's play a game.");
+                    Console.WriteLine("Enter a number from 1 - 100 and I'll show you a trick:");
+                    i = int.Parse(Console.ReadLine());
+                }
+                {
+                    {
+                        if (i % 2 != 0 && i < 60) // Odd condtions
+                            Console.WriteLine(i + " is an Odd number");
+                        else if (i % 2 != 0 && i >= 60) //Has errors because 64 prints odd and even
+                            Console.WriteLine(i + " is an Odd number over 60");
+
+                    }
+
+                    {
+                        if (i % 2 == 0 && i > 25) // Even conditions
+                            Console.WriteLine(i + " is an Even number");
+                        else if (i % 2 == 0 && i >= 2)
+                            Console.WriteLine(i + " is and Even number between 2 and 25");
+						else if (i % 2 == 0 && i <= 25)
+							Console.WriteLine(i + " is and Even number between 2 and 25");
+
+                    }
+
+                    {
+                        Console.WriteLine("Play again (y/n)?");
+                        answer = Console.ReadLine();
+                        //if (answer == "y")
+                        //Console.WriteLine("Enter a number from 1 - 100 and I'll show you a trick:");
+                        //i = int.Parse(Console.ReadLine());
+                        //else (answer == "n")
+                        //break;
+                    }
+                }
+            }
+        }
+
+    }
+}
