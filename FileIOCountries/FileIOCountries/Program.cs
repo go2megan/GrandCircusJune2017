c8cb94534c08f8187adf716e153ba455caca7873
Megan J. Norton <go2megan@gmail.com>
2017-07-14 07:42:50 -0400

    File IO

---
+++ FileIOCountries/FileIOCountries/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOCountries
+{
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            bool run = true;
+            while (run == true)
+            {
+                Console.WriteLine("Welcome to the Coutries Maintenance Application");//Title of the program            
+            }
+
+            {
+                Console.WriteLine("Enter 1 to see a list of countries, enter 2 to add to the list, enter 3 to exit");
+                string input = Console.ReadLine();
+
+
+                ReadaFile r = new ReadaFile ();
+                r.ReadaFile(input);
+                WriteFiles w = new WriteFiles();
+
+            }
+
+        }
+
+        public static ReadaFile(string input)
+        {
+            if (input == "1")
+            {
+                string filePath = @"C:\Users\go2me\Downloads\readCountries.txt";
+                StreamReader reader = new StreamReader(filePath);
+                string file = reader.ReadToEnd();
+                input = reader.ReadToEnd();
+                //Console.WriteLine(file);
+
+                try
+                {
+                    { 
+                        if (reader == null)
+                        {
+                            Console.WriteLine("empty file");
+                        }
+                        else if (reader != null)
+                        {
+                            Console.WriteLine("The contents of the are:" + reader);
+                        }
+                        return;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+
+        public void WriteaFile(string input)
+        {
+            if (input == "2")
+            {
+                string filePath = @"C:\Users\go2me\Downloads\readCountries.txt";
+                StreamWriter writer = new StreamWriter(filePath);
+
+                try
+                {
+                    writer = new StreamWriter(filePath);
+                    writer.Write("France");
+                    writer.WriteLine("Spain");
+                    writer.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+         run = Continue(); // For a continue loop
+        }
+        
+
+        public static Boolean Continue()
+        {
+            Boolean run;
+            Console.WriteLine("Read more files? Y/N");
+            String answer = Console.ReadLine();
+
+            if ((answer == "Y") || (answer == "y"))
+            {
+                run = true;
+            }
+            else if ((answer == "N") || (answer == "n"))
+            {
+                run = false;
+                Console.WriteLine("Thanks! Come back another time.");
+            }
+            else
+            {
+                Console.WriteLine("Oops! What was that? Y/N?");
+                run = Continue();
+            }
+            return run;
+        }
+    }
+}
---
+++ FileIOCountries/FileIOCountries/TommysCode.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FIleIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Tommy\Documents\Visual Studio 2015\Projects\Code Examples\FIleIO\FIleIO\file.txt";
+
+            StreamWriter writer;
+            StreamReader reader = new StreamReader(filePath);
+            string input;
+            try
+            {
+                string line = reader.ReadToEnd();
+                input = reader.ReadToEnd();
+                Console.WriteLine(input);
+                Console.WriteLine(line);
+                while (false)
+                {
+                    if (line == null)
+                    {
+                        break;
+                    }
+                    if (line.Contains("good"))
+                    {
+                        Console.WriteLine(line);
+                    }
+                    else if (line.Contains("was"))
+                    {
+                        Console.WriteLine("Hello world");
+                    }
+                    //  Console.WriteLine(line); // print line
+                }
+                reader.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            try
+            {
+                writer = new StreamWriter(filePath);
+                writer.Write("Word ");
+                writer.WriteLine("word 2");
+                writer.WriteLine("Line");
+                writer.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
